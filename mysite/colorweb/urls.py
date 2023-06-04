4fc8266967312ec7517ff60065441d78654500a6
DungDuong1002 <91011828+DungDuong1002@users.noreply.github.com>
2023-06-05 03:56:29 +0700

    Update urls.py

--- mysite/colorweb/urls.py
+++ mysite/colorweb/urls.py
@@ -10,13 +10,11 @@ router.register('my_image_conversion', views.MyImageAPI)
 
 urlpatterns = [
     path('', views.home, name='home'),
-    path('result', views.display_image, name='result'),
-    # path('upload', views.upload_image, name='image_upload'),
+    path('result/', views.display_image, name='result'),
     path('register/', views.register, name='register'),
     path('login/', auth_views.LoginView.as_view(template_name='image/login.html'), name='login'),
     path('logout/', auth_views.LogoutView.as_view(next_page='/'), name='logout'),
     path('colorize/', views.upload_image, name='upload_image'),
-    # path('api/image_conversion/', views.ImageConversionAPI.as_view(), name='image_conversion_api'),
     path('api/', include(router.urls))
 ]
 
